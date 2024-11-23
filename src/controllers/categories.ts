c7406e65e8ee8ffb1f12de228ff3a1e9893407b6
Agnès Cappello <senga.ds@gmail.com>
2024-11-23 22:57:41 +0100

    categories = post delate put et get ok

--- src/controllers/categories.ts
+++ src/controllers/categories.ts
@@ -13,5 +13,49 @@ export const categoriesController = {
             res.status(500).json({ message: "Erreur serveur" });
         }
     },
+    getDetails: async (req: Request, res: Response) => {
+        const id = parseInt(req.params.id)
+        try {
+            const result = await pool.query("SELECT * FROM Categories WHERE category_id = $1", [id]);
+            res.status(200).json({ category: result.rows[0] });  
+        } catch (error) {
+            console.error("Erreur dans le get categories details", error);
+            res.status(500).json({ message: "Erreur serveur" });
+        }
+    },
+    post : async (req: Request, res: Response) => {
+        try {
+            const { name } = req.body;
+            const result = await pool.query("INSERT INTO Categories (name) VALUES ($1) RETURNING *", [name]);
+            res.status(201).json({ category: result.rows[0] });
+        } catch (error) {
+            console.error("Erreur dans le post categories", error);
+            res.status(500).json({ message: "Erreur serveur coucou" });
+        }
+    },
+    delete: async (req: Request, res: Response) => {
+        const id = parseInt(req.params.id)
+        try {
+            await pool.query("DELETE FROM Categories WHERE category_id = $1", [id]);
+            console.log("id", id);  
+            res.status(200).json({ message: "categorie supprimée avec succès" });
+        } catch (error) {
+            console.error("Erreur dans le delete categories", error);
+            res.status(500).json({ message: "Erreur serveur"});
+        }
+    },
+    put: async (req: Request, res: Response) => {
+        const id = parseInt(req.params.id)
+        const {name} = req.body;
+        try {
+            await pool.query(
+                "UPDATE Categories SET name = $1 WHERE category_id = $2",
+                [name, id]);
+            res.status(200).json({ message: "categorie mise à jour avec succès" });
+        } catch (error) {
+            console.error("Erreur dans le MAJ categories", error);
+            res.status(500).json({ message: "Erreur serveur"});
+        }
+    }
 };
 
--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -6,9 +6,9 @@ import { categoriesController } from "../controllers/categories";
 export const router = Router();
 
 router.get("/", categoriesController.get);
-//router.get("/:id", categoriesController.getDetails); 
-//router.post("/", categoriesController.post); 
-// router.delete("/:id", ingredientsController.delete); 
-// router.put("/:id", ingredientsController.put);
+router.get("/:id", categoriesController.getDetails); 
+router.post("/", categoriesController.post); 
+router.delete("/:id", categoriesController.delete); 
+router.put("/:id", categoriesController.put);
 
 
