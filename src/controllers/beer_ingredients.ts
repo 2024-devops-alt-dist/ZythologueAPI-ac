2f464c241e43cb7560e9d8d3a4164684a6d0f5c2
Agnès Cappello <senga.ds@gmail.com>
2024-12-16 16:44:11 +0100

    controllers ok (sauf beer_ingredients) et swagger ok

--- src/controllers/beer_ingredients.ts
+++ src/controllers/beer_ingredients.ts
@@ -13,10 +13,21 @@ export const beer_ingredientsController = {
             res.status(500).json({ message: "Erreur serveur" });
         }
     },
+    getDetails: async (req: Request, res: Response) => {
+        const id = parseInt(req.params.id);
+        console.log("leeee id", id);
+        try {
+            const result = await pool.query("SELECT * FROM Beer_Ingredients WHERE beer_ingredient_id = $1", [id]);
+            res.status(200).json({ beer_ingredient: result.rows[0] });
+        } catch (error:any) {
+            console.error("Erreur dans le get Beer_Ingredients", error);
+            res.status(500).json({ error: error.message });
+        }
+    },
     post: async (req: Request, res: Response) => {
         try {
-            const { name, beer_id, ingredient_id } = req.body;
-            const result = await pool.query("INSERT INTO Beer_Ingredients (name, beer_id, ingredient_id) VALUES ($1, $2, $3) RETURNING *", [name, beer_id, ingredient_id]);
+            const { beer_id, ingredient_id, percentage } = req.body;
+            const result = await pool.query("INSERT INTO Beer_Ingredients (beer_id, ingredient_id, percentage) VALUES ($1, $2, $3) RETURNING *", [beer_id, ingredient_id, percentage]);
             res.status(201).json({ beer_ingredient: result.rows[0] });
         } catch (error: any) {
             console.error("Erreur dans le post Beer_Ingredients", error);
--- src/routes/beer_ingredients.ts
+++ src/routes/beer_ingredients.ts
@@ -6,6 +6,7 @@ import { beer_ingredientsController } from "../controllers/beer_ingredients";
 export const router = Router();
 
 router.get("/", beer_ingredientsController.get);
+router.get("/:id", beer_ingredientsController.getDetails); 
 router.post("/", beer_ingredientsController.post);
 
 
