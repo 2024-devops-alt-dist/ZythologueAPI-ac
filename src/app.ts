64d913cb48e9b92465c5a94d2bb986d41fe25293
Agnès Cappello <senga.ds@gmail.com>
2025-01-13 14:28:50 +0100

    fix bug cors

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,18 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './../swagger.json';
 
 import express, {Application} from "express";
+import cors from "cors";
+
+
+const corsOptions = {
+    origin: ['http://localhost:5000', 'http://localhost:5173'], 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+  };
+
 const app: Application = express();
 app.use(express.json());// accepter le format json sur les requetes
+app.use(cors(corsOptions));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
